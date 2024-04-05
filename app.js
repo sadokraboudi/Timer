d30131f5d4baf5d896bbc477814716395057107f
Sadok Raboudi <sadokraboudi78@gmail.com>
2024-04-05 08:14:36 +0100

    add the animation logic

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
-class Timer {
-    constructor(durationInput, startButton , pauseButton){
-        this.durationInput = durationInput; 
-        this.startButton = startButton; 
-        this.pauseButton = pauseButton; 
-   
-
-        this.startButton.addEventListener('click',this.start);
-        this.pauseButton.addEventListener('click',this.pause); 
-
-}  
-    
-   
-   
-    start = ()=> {
-        this.tick(); 
-        this.intervalId = setInterval(this.tick, 1000); 
-
-    }
-    pause = ()=> {
-        clearInterval(this.intervalId); 
-        
-    }
-    tick = () => {
-        if (this.timeRemaining <= 0)
-            this.pause(); 
-        else
-            this.timeRemaining = this.timeRemaining -1; 
- 
-    }
-    get timeRemaining(){
-        return parseFloat(this.durationInput.value); 
-    }
-    set timeRemaining(time){
-        this.durationInput.value = time; 
-    }
-   
-}
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start'); 
-const pauseButton = document.querySelector('#pause'); 
-
-const timer = new Timer(durationInput,startButton,pauseButton);
+const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('#circle');
+const perimeter = circle.getAttribute('r') * 2 * Math.PI; 
+circle.setAttribute('stroke-dasharray', perimeter);
+let duration; 
+const timer = new Timer(durationInput,startButton,pauseButton, {
+    onStart(totalDuration){
+        duration = totalDuration; 
+    },
+    onTick(timeRemaining){
+        circle.setAttribute('stroke-dashoffset', 
+        perimeter * timeRemaining / duration - perimeter );
+    },
+    onComplete(){
+        circle.setAttribute('stroke-dashoffset',0)
+    }
+});
